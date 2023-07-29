9cec126cd49bf53b5706eb2dd410df9139986e60
Niyaz N <niyaznouzz@gmail.com>
2023-07-29 20:58:46 +0530

    sum addedd

--- java.js
+++ java.js
@@ -1,1 +1,19 @@
 document.write("learn Javascript");
+//alert("Do you want to continue");
+//variables and datatypes 
+
+//string
+var person = "NIYAZ ";
+var person1 = "chinna";
+document.write(person);
+
+//number
+var num = 20 ;
+var num2 = 20 ;
+
+//sum
+var a =10;
+var b= 30;
+
+sum =a+b;
+console.log(sum);
